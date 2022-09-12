3e69e826259f74b20639d3c75605668f6e9e75ab
LeHoser <nathan.lloyd.7003@gmail.com>
2022-09-12 11:46:54 -0300

    Initial commit

---
+++ Hello World GiHub 2 Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_GiHub_2_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
